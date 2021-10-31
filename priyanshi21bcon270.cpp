a04979334ede96f12ed9a85a79ea3d466223a9cd
hellopriyanshi <91782970+hellopriyanshi@users.noreply.github.com>
2021-10-30 22:31:37 -0700

    Create priyanshi21bcon270.cpp
    this is my first PR in c++ language .

---
+++ priyanshi21bcon270.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+  clrscr();
+  int a,b,c,d=0;
+  cout<<"enter the numbers to be swapped :"<<"/n";
+  cin>>a>>"/n">>b>>"/n">>c>>"/n";
+  d=a;
+  b=c;
+  a=b;
+  cout<<"the swapped numbers are :"<<"/n"<<d<<"/n"<<b<<"/n"<<a<<"/n";
+  getch();
+}
+  
