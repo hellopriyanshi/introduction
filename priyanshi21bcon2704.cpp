db033ae11c0e91abe49444263b7cc595bfbbf512
hellopriyanshi <91782970+hellopriyanshi@users.noreply.github.com>
2021-10-31 05:34:43 -0700

    Create priyanshi21bcon2704.cpp
    program to find out whether a citizen is eligible to vote or not .

---
+++ priyanshi21bcon2704.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+  clrscr();
+  int age;
+  cout<<"enter the age of the citizen :"<<"\n";
+  cin>>age;
+  if(age>=18)
+  {cout<<"eligible to vote ";}
+  else{cout<<"not eligible to vote ";}
+  getch();
+}
