b21fd3b2bc5479cd8df80791415149023182b674
hellopriyanshi <91782970+hellopriyanshi@users.noreply.github.com>
2021-10-30 22:47:36 -0700

    Create priyanshi21bcon2701.cpp
    code to swap three variables in c++ language.

---
+++ priyanshi21bcon2701.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+  clrscr();
+  int a,b,c,d=0;
+  cout<<"enter the numbers to be swapped :"<<"\n";
+  cin>>a>>"\n">>b>>"\n">>c"\n";
+  d=a;
+  b=c;
+  a=b;
+  cout<<"the swapped numbers are :<<"\n"<<d<<"\n"<<b<<"\n"<<a<<"\n";
+    getch();
+}
